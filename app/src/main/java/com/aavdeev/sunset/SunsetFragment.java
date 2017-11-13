f9eeaca798a8d1592717cb59d3e05b01076950b5
avdey <avdey87@gmail.com>
2017-11-13 17:11:14 +0300

    578 Выбор интерполятора

--- app/src/main/java/com/aavdeev/sunset/SunsetFragment.java
+++ app/src/main/java/com/aavdeev/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.aavdeev.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SunsetFragment extends Fragment {
+    private View mScenceView;
+    private View mSunView;
+    private View mSkyView;
+
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
     }
@@ -18,7 +23,29 @@ public class SunsetFragment extends Fragment {
             container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sunset,
                 container, false);
+
+        mScenceView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mScenceView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAnimation();
+            }
+        });
         return view;
+    }
+    //
+    private void startAnimation() {
+        float sunYstart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYstart, sunYEnd)
+                .setDuration(3000);
+        heightAnimator.start();
+
     }
 
 }
